c1bc29bb61020cb2e3193f6a19db32c0bb3b344b
syo <connorhadley@gmail.com>
2018-02-06 15:01:21 -0500

    basic project structure

---
+++ src/TokenManager.ts
@@ -0,0 +1,18 @@
+//import stuff
+
+/**
+ * Some interface for the token manager.
+ */
+export interface SomeInterface {
+    /**
+     * Description of what someVar does.
+     */
+    someVar?: String;
+}
+
+/**
+ * A free ERC-20 token manager
+ */
+export class TokenManager {
+    //need a constructuctor
+}
